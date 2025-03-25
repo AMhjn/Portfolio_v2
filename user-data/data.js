fcadae7f5990917ede33da83c1f4e232987be9e8
Abhir Mahajan <abhirmhjn@gmail.com>
2025-03-25 00:51:51 -0400

    Updated Experience

--- user-data/data.js
+++ user-data/data.js
@@ -1,6 +1,6 @@
 export const bio = [
   "Hello, I'm Abhir Mahajan!",
-  "<strong>Frontend Software Engineer</strong> with 4+ years of experience building scalable, user-focused web applications.",
+  "<strong>Full Stack Software Engineer</strong> with 2+ years of experience building scalable, high-performance applications that solve real-world problems.",
   "Skilled in Angular, TypeScript, and performance optimization, I aim to leverage my expertise in delivering innovative solutions that drive measurable business outcomes.",
   "Passionate about mentoring, teamwork, and continuous improvement in engineering processes.",
 ];
@@ -14,79 +14,62 @@ export const skills = [
 
 export const experience = [
   {
-    title: "Shiprocket Private Limited",
-    duration: "September 2023 - Present",
+    title: "Volkswagen Group Technology Solutions",
+    duration: "August 2021 - July 2023",
     subtitle: "Software Engineer",
     details: [
-      "Developed a design system using Stencil and Storybook that provides consistent UI components across platforms, ensuring a cohesive design.",
-      "Developed a dynamic support ticket panel that renders subcategories based on component IDs, handling up to 100 AWBs simultaneously.",
-      "Migrated 30+ modules from AngularJS to Angular 15 using Generative AI tools, reporting directly to the VP of Engineering.",
-      "Designed and implemented advanced features such as keyboard shortcuts, improving operational efficiency by 15 percent.",
-      "Took ownership of OKRs, collaborated with the product team, and mentored two junior developers, cutting their code review iterations and ensuring on-time feature delivery.",
+      "Engineered and deployed the 'International Tax Documents' portal using Angular, Java, Spring Boot, and MySQL, automating document submission, approval, and delegation workflows, decreasing manual effort, and achieving 70% client adoption.",
+      "Led a large-scale data migration from 'IRIS' MySQL database to 'IRIS 2.0' database, normalizing 100+ complex schemas, ensuring 100% data integrity, and accomplishing a seamless transition with less than 1% downtime and zero data loss.",
+      "Designed UI and REST APIs for 'IRIS' system, optimizing invoice creation, approval, and feedback workflows using Angular, Node.js, Express.js, and MongoDB, driving a 40% increase in user adoption and reducing manual processing time.",
+      "Mentored 3 interns, improving onboarding efficiency and integration time by 50%, while conducting detailed code reviews, ensuring best coding practices, and improving system reliability.",
+      "Implemented comprehensive logging architecture across Microservices, accelerating issue diagnosis time by 60%, boosting system reliability by 40%, and enhancing customer satisfaction through faster issue resolution.",
+      "Earned prestigious 'Debutant' and 'Instapat' awards for problem-solving and innovative mindset that boosted customer engagement and increased IRIS project efficiency by 40%, demonstrating ability to drive project success",
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "AngularJs"],
+    tags: ["Java", "Spring Boot", "MySQL", "Node.js", " Express.js", "MongoDB", "Angular", "REST APIs", "Microservices"],
     icon: "truck ",
   },
+
   {
-    title: "Biofourmis India Private Limited",
-    duration: "April 2022 - Jul 2023",
-    subtitle: "Software Engineer",
+    title: "Paytm",
+    duration: "February 2021 - July 2021",
+    subtitle: "Software Engineer Intern",
     details: [
-      "Spearheaded the implementation of micro-frontends using Module Federation, enabling modular and scalable architecture.",
-      "Earned the Biofourmis Bravo Award for upgrading the application to support flexible operations across multiple geographies.",
-      "Enhanced performance and user experience by integrating Twilio-Video, Countly, and NGXS WebSocket for real-time data management.",
+      "Enhanced Paytm’s mobile app stability by identifying and resolving critical performance issues, resulting in a 25% reduction in post-release defects and a smoother user experience.",
+      "Automated and optimized testing processes using Java, Selenium, and Appium, executing 1,000+ test cases across Android and iOS in an Agile environment, diminishing bug detection time by 15% and strengthening overall app quality.",
     ],
     tags: [
-      "JavaScript",
-      "Angular",
-      "RxJS",
-      "NGXS",
-      "TypeScript",
-      "RxState",
-      "Webpack",
-      "Optimization",
+      "Java",
+      "Selenium",
+      "Appium",
+      "Manual Testing",
+      "Automation Testing",
+      "Mobile App Testing",
     ],
-    icon: "heartbeat",
+    icon: "mobile-alt",
   },
-  {
-    title: "Novopay Solutions Private Limited",
-    duration: "June 2020 - April 2022",
-    subtitle: "Software Engineer",
-    details: [
-      "Built critical banking features including AePS services (eKYC, cash withdrawal, balance inquiry) used by over 1,00,000 retailers.",
-      "Developed QR code integration for wallet money loading, streamlining retailer workflows and reducing errors by 25 percent.",
-      "Integrated Clevertap analytics, Freshdesk ticketing, and chatbot services, enhancing user support and engagement.",
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-    icon: "qrcode",
-  },
-  {
-    title: "ThinkPedia LLP",
-    duration: "May 2019 - June 2019",
-    subtitle: "SDE Intern",
-    details: [
-      `Designed and developed a social media post management panel, improving collaboration efficiency by 50 percent.`,
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-    icon: "group",
-  },
+  
 ];
 
-export const trekking = [
-  "<strong>Kheerganga Trek</strong>, Himachal Pradesh (9,711 feet)",
-  "<strong>Triund Trek</strong>, Himachal Pradesh (9,350 feet)",
-  "<strong>Kedarkantha Trek</strong>, Uttarakhand (12,500 feet)",
-  "<strong>Jalori Pass Trek</strong>, Himachal Pradesh (10,800 feet)",
-  "<strong>Vaishno Devi Trek</strong>, Jammu & Kashmir (5,200 feet)",
-];
+// export const trekking = [
+//   "<strong>Kheerganga Trek</strong>, Himachal Pradesh (9,711 feet)",
+//   "<strong>Triund Trek</strong>, Himachal Pradesh (9,350 feet)",
+//   "<strong>Kedarkantha Trek</strong>, Uttarakhand (12,500 feet)",
+//   "<strong>Jalori Pass Trek</strong>, Himachal Pradesh (10,800 feet)",
+//   "<strong>Vaishno Devi Trek</strong>, Jammu & Kashmir (5,200 feet)",
+// ];
 
 export const education = [
     {
       title: "Master of Science in Computer & Information Science",
-      duration: "",
-      subtitle: "Syracuse University,New York",
+      duration: "August 2023 - May 2025",
+      subtitle: "Syracuse University, New York",
       details: [],
-      tags: ["Physics", "Chemistry", "Mathematics"],
+      tags: ["Advanced Data Structures & Algorithms",
+      "Object Oriented Design",
+      "Database Management System",
+      "Artificial Intelligence",
+      "Computer Architecture",
+      "Android Programming",],
       icon: "graduation-cap",
     },
     {
@@ -98,9 +81,9 @@ export const education = [
       "Data Structures & Algorithms",
       "Operating Systems",
       "Database Management System",
+      "Object Oriented Programming",
       "Computer Networks",
-      "Cloud Computing",
-    ],
+      "Software Engineering",],
     icon: "book",
   },
   
